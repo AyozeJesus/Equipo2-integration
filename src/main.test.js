aa4d17b6018f6fb7d62246e212e69cc60d051d0c
AyozeJesus <ayozejesusleonsantos@gmail.com>
2023-09-29 20:50:31 +0100

    prueba

--- src/main.test.js
+++ src/main.test.js
@@ -1,8 +1,8 @@
-/* import { describe, it, expect } from "vitest"
+import { describe, it, expect } from "vitest"
 import request from "supertest"
 import { app } from "./main"
 describe("NewRegisterController", () => {
-  it.skip("debería registrar un nuevo usuario", async () => {
+  it("debería registrar un nuevo usuario", async () => {
     const userData = {
       name: "Johnaa Doe",
       email: "johnas@example.com",
@@ -15,4 +15,3 @@ describe("NewRegisterController", () => {
     expect(response.status).toBe(200)
   })
 })
- */
