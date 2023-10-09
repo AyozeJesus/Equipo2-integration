3a805db9cb180dc3bd45b9f9f929a5bb73019710
AyozeJesus <ayozejesusleonsantos@gmail.com>
2023-10-09 19:02:09 +0100

    fix slint

--- src/infrastructure/API/main.js
+++ src/infrastructure/API/main.js
@@ -6,6 +6,7 @@ import { EmailSenderMock } from "../EmailSender/EmailSenderMock.js"
 import { IdGeneratorNode } from "../IdGenerator/IdGeneratorNode.js"
 import { errorCodeToStatus } from "./errorCodeToStatus.js"
 
+// eslint-disable-next-line no-unused-vars
 export function errorHandler(err, req, res, next) {
   const statusCode = errorCodeToStatus(err.code)
   res.status(statusCode).json({ code: err.code, error: err.message })
